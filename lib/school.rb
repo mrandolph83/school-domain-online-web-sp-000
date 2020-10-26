681eafd0457f21a3f77678c5fa12548e55eeba98
Matt Smith <mrandolph83@gmail.com>
2020-10-26 01:48:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,5 @@
 # code here!
 class School
-  attr_accessrod :name, :roster
 
     def initialize(name)
       @name = name
@@ -10,13 +9,11 @@ class School
     def roster
       @roster
     end
-end
 
-def add_student(name, grade)
-  roster[grade] = [name]
+    def add_student(name, grade)
+      roster[grade] = [name]
 # Adds a student
 # Adds multiple students to a class (grade)
 # Add students to different grades
-
-
+    end
 end
