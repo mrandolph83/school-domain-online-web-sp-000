184497d917086104a1aef6b93b11b922d3a30492
Matt Smith <mrandolph83@gmail.com>
2020-10-26 01:46:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,3 +10,12 @@ class School
       @roster
     end
 end
+
+def add_student(name, grade)
+  roster[grade] = [name]
+# Adds a student
+# Adds multiple students to a class (grade)
+# Add students to different grades
+
+
+end
