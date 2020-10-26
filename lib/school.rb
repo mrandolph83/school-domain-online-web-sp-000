32e66414716f36bb00c1b47019d32098534a7afb
Matt Smith <mrandolph83@gmail.com>
2020-10-26 01:49:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,10 @@
 # code here!
 class School
+  attr_accessor :name, :roster
 
     def initialize(name)
       @name = name
       @roster = {}
-    end
-
-    def roster
-      @roster
     end
 
     def add_student(name, grade)
