ef13883dadaad91f43a8fb44a27d3d9a250fb593
Matt Smith <mrandolph83@gmail.com>
2020-10-26 02:01:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,9 @@ class School
     end
 
     def add_student(name, grade)
+      if roster.has_key?(grade)
+        roster[grade] << name
+      else
       roster[grade] = [name]
 # Adds a student
 # Adds multiple students to a class (grade)
