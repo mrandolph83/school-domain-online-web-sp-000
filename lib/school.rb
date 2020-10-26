ef393cc3b6ebde2d8c4f5d25967da914eb10cc8f
Matt Smith <mrandolph83@gmail.com>
2020-10-26 02:01:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,6 @@ class School
         roster[grade] << name
       else
       roster[grade] = [name]
-# Adds a student
-# Adds multiple students to a class (grade)
-# Add students to different grades
     end
+  end
 end
